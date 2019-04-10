6687cc82c312a0d86533981ae70690d76162401a
jayathungek <jayathunge.work@gmail.com>
2019-04-10 15:33:12 +0100

    successfully able to record clean audio at 16bit depth

---
+++ src/wav.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+typedef struct
+{
+    char RIFF_marker[4];
+    uint32_t file_size;
+    char filetype_header[4];
+    char format_marker[4];
+    uint32_t data_header_length;
+    uint16_t format_type;
+    uint16_t number_of_channels;
+    uint32_t sample_rate;
+    uint32_t bytes_per_second;
+    uint16_t bytes_per_frame;
+    uint16_t bits_per_sample;
+} WaveHeader;
+
