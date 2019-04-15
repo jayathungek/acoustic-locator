9e7adae9b88e0e8a99c873724ca03e567f515ab5
jayathungek <jayathunge.work@gmail.com>
2019-04-15 17:11:35 +0100

    Move makefile

--- test/runtests.c
+++ test/runtests.c
@@ -2,22 +2,17 @@
 
 #include <stdio.h>
 #include "minunit.h"
-#include "../src/locator.c"
+#include "../src/utils.h"
+#include "../src/servo.h"
+
+#include "../src/utils.c"
+#include "../src/servo.c"
 
 
 mu_suite_start();
 int foo = 0;
 tests_run = 0;
 
-static char * test_alsaSetup() {
-	mu_assert("error, foo != 7", foo == 7);
-	return 0;
-}
-
-static char * test_wiringPiSetup() {
-	mu_assert("error, foo != 7", foo == 7);
-	return 0;
-}
 
 static char * test_16bitToSignedIntConv() {
 	mu_assert("error, foo != 7", foo == 7);
@@ -29,12 +24,28 @@ static char * test_motorSafety() {
 	return 0;
 }
 
-static char * all_tests() {
-	mu_run_test(test_alsaSetup);
-	mu_run_test(test_wiringPiSetup);
-	mu_run_test(test_16bitToSignedIntConv);
-	mu_run_test(test_motorSafety);
+static char * test_parabolaSafety() {
+	mu_assert("error, foo != 7", foo == 7);
 	return 0;
 }
 
+static char * all_tests() {
+	mu_run_test(test_16bitToSignedIntConv);
+	mu_run_test(test_motorSafety);
+	mu_run_test(test_parabolaSafety);
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	char *result = all_tests();
+	if (result != 0) {
+	 printf("%s\n", result);
+	}
+	else {
+	 printf("ALL TESTS PASSED\n");
+	}
+	printf("Tests run: %d\n", tests_run);
+
+	return result != 0;
+}
 
